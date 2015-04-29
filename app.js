d4c168bbcfbcf0508d558bba703ad1232a528dc2
Golo Roden <webmaster@goloroden.de>
2015-04-29 21:08:09 +0200

    Implemented all base functions.

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ var Chord = require('./lib/Chord');
 
 var chord = new Chord({
   host: 'localhost',
-  port: 3000
+  port: (process.env.PORT - 0) || 3000
 });
+
+setInterval(function () {
+  console.log();
+  console.log('Self: ', chord.self.host, chord.self.port, chord.self.id);
+  console.log('Succ: ', chord.successor.host, chord.successor.port, chord.successor.id);
+  if (chord.predecessor) {
+    console.log('Pred: ', chord.predecessor.host, chord.predecessor.port, chord.predecessor.id);
+  } else {
+    console.log('Pred: n/a');
+  }
+  console.log();
+}, 2 * 1000);
