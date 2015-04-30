be10ccad5f13cde4caaf6f6e79d2981c84bed43f
Golo Roden <webmaster@goloroden.de>
2015-04-30 15:30:44 +0200

    Fixed ESLint errors.

---
+++ test/ChordTests.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('assertthat');
+
+var Chord = require('../lib/Chord');
+
+suite('Chord', function () {
+  test('is a function.', function (done) {
+    assert.that(Chord).is.ofType('function');
+    done();
+  });
+});
