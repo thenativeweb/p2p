ab0377e58d3e443bc0dace181e3d2e4ddc7a7ae7
Golo Roden <webmaster@goloroden.de>
2015-04-30 11:45:57 +0200

    Implemented fix-predecessor.

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ var chord = new Chord({
 
 setInterval(function () {
   console.log();
-  console.log('Self: ', chord.self.host, chord.self.port, chord.self.id);
-  console.log('Succ: ', chord.successor.host, chord.successor.port, chord.successor.id);
   if (chord.predecessor) {
     console.log('Pred: ', chord.predecessor.host, chord.predecessor.port, chord.predecessor.id);
   } else {
     console.log('Pred: n/a');
   }
-  console.log();
+  console.log('Self: ', chord.self.host, chord.self.port, chord.self.id);
+  console.log('Succ: ', chord.successor.host, chord.successor.port, chord.successor.id);
 }, 2 * 1000);
