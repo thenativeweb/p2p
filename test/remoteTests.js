6d49150f5e929e96f4dec8938688ea3f072caa3c
Golo Roden <webmaster@goloroden.de>
2015-05-21 20:47:56 +0200

    Added remote tests.

--- test/remoteTests.js
+++ test/remoteTests.js
@@ -1,9 +1,92 @@
 'use strict';
 
-// var assert = require('assertthat');
+var assert = require('assertthat'),
+    nock = require('nock');
 
-// var remote = require('../lib/remote');
+var remote = require('../lib/remote');
 
 suite('remote', function () {
-  test.skip('...');
+  test('is a function.', function (done) {
+    assert.that(remote).is.ofType('function');
+    done();
+  });
+
+  test('throws an error if host is missing.', function (done) {
+    assert.that(function () {
+      remote();
+    }).is.throwing('Host is missing.');
+    done();
+  });
+
+  test('throws an error if port is missing.', function (done) {
+    assert.that(function () {
+      remote('localhost');
+    }).is.throwing('Port is missing.');
+    done();
+  });
+
+  test('returns an object.', function (done) {
+    assert.that(remote('localhost', 3000)).is.ofType('object');
+    done();
+  });
+
+  suite('run', function () {
+    test('is a function.', function (done) {
+      assert.that(remote('localhost', 3000).run).is.ofType('function');
+      done();
+    });
+
+    test('throws an error if the function to be called is missing.', function (done) {
+      assert.that(function () {
+        remote('localhost', 3000).run();
+      }).is.throwing('Function is missing.');
+      done();
+    });
+
+    test('throws an error if the callback is missing.', function (done) {
+      assert.that(function () {
+        remote('localhost', 3000).run('rpc');
+      }).is.throwing('Callback is missing.');
+      done();
+    });
+
+    test('calls the remote function with the given arguments.', function (done) {
+      var scope = nock('https://localhost:3000').
+        post('/rpc', { foo: 'ping' }).
+        reply(200, { foo: 'pong' });
+
+      remote('localhost', 3000).run('rpc', { foo: 'ping' }, function (err, result) {
+        assert.that(err).is.null();
+        assert.that(result).is.equalTo({ foo: 'pong' });
+        assert.that(scope.isDone()).is.true();
+        done();
+      });
+    });
+
+    test('calls the remote function with an empty object if no arguments are given.', function (done) {
+      var scope = nock('https://localhost:3000').
+        post('/rpc', {}).
+        reply(200, { foo: 'pong' });
+
+      remote('localhost', 3000).run('rpc', function (err, result) {
+        assert.that(err).is.null();
+        assert.that(result).is.equalTo({ foo: 'pong' });
+        assert.that(scope.isDone()).is.true();
+        done();
+      });
+    });
+
+    test('returns an error if a status code other than 200 is returned.', function (done) {
+      var scope = nock('https://localhost:3000').
+        post('/rpc', {}).
+        reply(500);
+
+      remote('localhost', 3000).run('rpc', function (err) {
+        assert.that(err).is.not.null();
+        assert.that(err.message).is.equalTo('Unexpected status code 500 when running rpc.');
+        assert.that(scope.isDone()).is.true();
+        done();
+      });
+    });
+  });
 });
