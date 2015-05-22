a603a18cdf1a60fee0db05420f1425ff40039236
Golo Roden <webmaster@goloroden.de>
2015-05-22 11:29:16 +0200

    Added a better sample.

---
+++ samples/client.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var url = require('url');
+
+var flaschenpost = require('flaschenpost'),
+    processEnv = require('processenv'),
+    request = require('request'),
+    uuid = require('uuidv4');
+
+var logger = flaschenpost.getLogger();
+
+var port = processEnv('PORT') || 3000;
+
+var job = {
+  id: process.argv[2] || uuid(),
+  data: process.argv[3] || 'foo'
+};
+
+request.post(url.format({
+  protocol: 'http',
+  hostname: 'localhost',
+  port: port,
+  pathname: '/get-node-for'
+}), {
+  body: { value: job.id },
+  json: true
+}, function (errGetNodeFor, resGetNodeFor, node) {
+  if (errGetNodeFor) {
+    logger.fatal('Failed to get the responsible node.', err);
+    /*eslint-disable no-process-exit*/
+    process.exit(1);
+    /*eslint-enable no-process-exit*/
+  }
+
+  request.post(url.format({
+    protocol: 'http',
+    hostname: node.host,
+    port: node.port,
+    pathname: '/job'
+  }), {
+    body: job,
+    json: true
+  }, function (errJob, resJob) {
+    if (errJob || (resJob.statusCode !== 200)) {
+      logger.fatal('Failed to send job.', err);
+      /*eslint-disable no-process-exit*/
+      process.exit(1);
+      /*eslint-enable no-process-exit*/
+    }
+
+    logger.info('Sent job {{job.id}} to {{target.host}}:{{target.port}}.', {
+      job: job,
+      target: node
+    });
+  });
+});
---
+++ samples/server.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var fs = require('fs'),
+    http = require('http'),
+    path = require('path');
+
+var bodyParser = require('body-parser'),
+    express = require('express'),
+    flaschenpost = require('flaschenpost'),
+    processEnv = require('processenv');
+
+var Chord = require('../lib/Chord');
+
+var httpPort = processEnv('HTTP_PORT') || 3000,
+    p2pPort = processEnv('P2P_PORT') || 3001,
+    p2pPortJoin = processEnv('P2P_PORT_JOIN') || p2pPort,
+    serviceInterval = processEnv('SERVICE_INTERVAL') || '30s';
+
+var certificate = fs.readFileSync(path.join(__dirname, '..', 'keys', 'localhost.selfsigned', 'certificate.pem')),
+    privateKey = fs.readFileSync(path.join(__dirname, '..', 'keys', 'localhost.selfsigned', 'privateKey.pem'));
+
+var app,
+    chord,
+    logger = flaschenpost.getLogger();
+
+/*eslint-disable no-process-env*/
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+/*eslint-enable no-process-env*/
+
+chord = new Chord({
+  host: 'localhost',
+  port: p2pPort,
+  privateKey: privateKey,
+  certificate: certificate,
+  metadata: {
+    host: 'localhost',
+    port: httpPort
+  },
+  serviceInterval: serviceInterval
+});
+
+chord.on('changed-successor', function (successor) {
+  logger.debug('Changed successor.', {
+    successor: successor,
+    status: chord.status()
+  });
+});
+
+chord.on('changed-predecessor', function (predecessor) {
+  logger.debug('Changed predecessor.', {
+    predecessor: predecessor,
+    status: chord.status()
+  });
+});
+
+setTimeout(function () {
+  chord.join({ host: 'localhost', port: p2pPortJoin }, function (err) {
+    if (err) {
+      logger.fatal('Failed to join.', err);
+      /*eslint-disable no-process-exit*/
+      process.exit(1);
+      /*eslint-enable no-process-exit*/
+    }
+
+    app = express();
+
+    app.use(bodyParser.json());
+
+    app.post('/get-node-for', function (req, res) {
+      chord.getNodeFor(req.body.value, function (err, node, metadata) {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        res.send(metadata);
+      });
+    });
+
+    app.post('/job', function (req, res) {
+      logger.info('Received job.', req.body);
+      res.sendStatus(200);
+    });
+
+    http.createServer(app).listen(httpPort);
+  });
+}, 1000);
