04613373737a9770063846112a08fb76841d006c
Golo Roden <webmaster@goloroden.de>
2015-07-16 20:43:13 +0200

    Made integration test better readable.

--- integration/joinTests.js
+++ integration/joinTests.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var assert = require('assertthat');
+var assert = require('assertthat'),
+    async = require('async');
 
 var createPeers = require('./createPeers');
 
@@ -12,29 +13,25 @@ suite('join', function () {
   this.timeout(5 * 60 * 1000);
 
   test('connects 2 peers.', function (done) {
-    createPeers({ count: 2 }, function (errCreatePeers, peers, env) {
-      assert.that(errCreatePeers).is.null();
-
-      env.waitUntil(peers).have('status').equalTo({ status: 'lonely' }, function (errWaitUntil1) {
-        assert.that(errWaitUntil1).is.null();
-
-        peers[0].join(peers[1], function (errJoin) {
-          assert.that(errJoin).is.null();
-
-          env.waitUntil(peers).have('status').equalTo({ status: 'joined' }, function (errWaitUntil2) {
-            assert.that(errWaitUntil2).is.null();
-
-            peers[0].predecessor(function (errPredecessor1) {
-              assert.that(errPredecessor1).is.null();
-
-              env.stop(peers, function (errStop) {
-                assert.that(errStop).is.null();
-                done();
-              });
-            });
-          });
-        });
-      });
+    createPeers({ count: 2 }, function (err, peers, env) {
+      assert.that(err).is.null();
+      async.series([
+        function (callback) {
+          env.waitUntil(peers).have('status').equalTo({ status: 'lonely' }, callback);
+        },
+        function (callback) {
+          peers[0].join(peers[1], callback);
+        },
+        function (callback) {
+          env.waitUntil(peers).have('status').equalTo({ status: 'joined' }, callback);
+        },
+        function (callback) {
+          peers[0].predecessor(callback);
+        },
+        function (callback) {
+          env.stop(peers, callback);
+        }
+      ], done);
     });
   });
 });
