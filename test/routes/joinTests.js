97e3e43286c91328170aaf83a43e1b79c178cbb0
Golo Roden <webmaster@goloroden.de>
2015-08-08 11:20:44 +0200

    A peer joining itself is a no-op. Closes thenativeweb/org#395

--- test/routes/joinTests.js
+++ test/routes/joinTests.js
@@ -74,6 +74,22 @@ suite('join', function () {
         });
     });
 
+    test('does nothing if the peer is told to join itself.', function (done) {
+      var findSuccessor = nock('https://localhost:3000').post('/find-successor').reply(500);
+
+      request(peer.app).
+        post('/join').
+        set('content-type', 'application/json').
+        send({ host: 'localhost', port: 3000 }).
+        end(function (err, res) {
+          assert.that(err).is.null();
+          assert.that(res.statusCode).is.equalTo(200);
+          assert.that(findSuccessor.isDone()).is.false();
+          nock.cleanAll();
+          done();
+        });
+    });
+
     test('asks the remote peer for the local peer\'s successor.', function (done) {
       var remotePeer = new mocks.LonelyPeer({ host: 'localhost', port: 4000 });
       var remotePeerFindSuccessor = nock('https://localhost:4000').post('/find-successor').reply(200, remotePeer.self);
